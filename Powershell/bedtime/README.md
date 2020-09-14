bc144664b88944def4aa17b2143997d938c25218
ciwen3 <55612276+ciwen3@users.noreply.github.com>
2020-09-14 12:28:10 -0700

    modified:   Powershell/bedtime/Get2Run.txt 	modified:   Powershell/bedtime/README.md 	modified:   Powershell/bedtime/bedtime.ps1

--- Powershell/bedtime/README.md
+++ Powershell/bedtime/README.md
@@ -4,6 +4,14 @@
 #
 used to power down my kids computers at a set time. 
 
+# Setup
+place a copy of the script in C:\Windows
+Open GPO editor: 
+User Configurations > Windows Settings > Scripts (Logon / Logoff) > Logon
+Add: 
+Script Name: C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+Script Parameters: -ExecutionPolicy bypass -file C:\Windows\bedtime.ps1
+
 
 # All of my Public projects, opinions and advice are offered “as-is”, without warranty, and disclaiming liability for damages resulting from using any of my software or taking any of my advice.
 
--- Powershell/bedtime/bedtime.ps1
+++ Powershell/bedtime/bedtime.ps1
@@ -6,22 +6,30 @@ add-type -assemblyname presentationcore,presentationframework
 
 # start a never ending while loop
 while ($x -eq 1) {
+
     # check to make sure things were working
     # write-host('x = 1')
+
     # start small sleep timer (incase I need to stop it) 
-    Start-Sleep -s 150
+    # Start-Sleep -s 150
+
     # grab the current hours in military time and set as a variable
     $time = get-date -Format "HH"
+
     # another check to make sure it works
     # write-host($time)
+
     # if loop to check if it is bed time
-    # if variable time is greater than or equal to 9 
-    # OR if variable time is less than or equal to 4 
-    if ($time -ge 17 -Or $time -le 4) {
+    # if variable time is greater than or equal to 9pm (21:00)
+    # OR if variable time is less than or equal to 4am (04:00)
+    if ($time -ge 21 -Or $time -le 4) {
+
         # popuo message box warning it is bedtime
-        [System.Windows.MessageBox]::Show('It is Bedtime!\nSay good night to your friends.\nWe love you.\nNow go to sleep.\nThis Computer will Shutdown in 5 minutes!!')
+        [System.Windows.MessageBox]::Show('It is Bedtime! Say good night to your friends. We love you. Now go to sleep. This Computer will Shutdown in 5 minutes!!')
+
         # 5 minute timer
         Start-Sleep -s 300
+
         # Force shutdown the computer
         Stop-Computer -Force
     }
