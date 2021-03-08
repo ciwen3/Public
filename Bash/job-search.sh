325fcaef2d8a38e285856f3d759346e61f7f1521
ciwen3 <55612276+ciwen3@users.noreply.github.com>
2021-03-08 01:22:13 -0800

    Create job-search.sh

---
+++ Bash/job-search.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for newjob in $(cat jobsearch); do 
+	if [[ work-life-ballence -gt mental-health-needs ]] 
+    then
+	    if [[ pay-benefits -gt cost-of-living ]] 
+        then
+	        if [[ job-education -eq my-interests ]] 
+            then
+	            if [[ job-advancement -eq deserverd ]] 
+                then
+	                if [[ $newjob -eq expectations ]]
+                    then
+                        echo "I accept $newjob" > response-email
+                        sendmail newboss@greatjob.com < response-email
+                    fi
+                fi
+            fi
+        fi
+    fi
+done
