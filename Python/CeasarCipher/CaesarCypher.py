a3b2289b7563c48b56845767463b9c2097408c1f
ciwen3 <55612276+ciwen3@users.noreply.github.com>
2022-09-29 00:36:54 -0700

    Add files via upload

---
+++ Python/CeasarCipher/CaesarCypher.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def ceasar( offset ):
+	newmsg = ''
+	msg = input('Please enter a message to encrypt: ').lower()
+	for character in msg:
+		if character in alphabet:
+			position = alphabet.find(character)
+			newposition = (position + offset) % 26
+			newcharacter = alphabet[newposition]
+			newmsg += newcharacter
+		else:
+			newmsg += character
+	print('the encrypted message is: ', newmsg)
+	input('Press Enter to Close Window')
+
+startmsg = int(input('Do you want to Encrypt or Decrypt? \n 1 for Encrypt \n 2 for Decrypt \n 3 to Close the Program \n'))
+
+offset = int(input('choose cipher offset: '))
+
+if startmsg == 1:
+	ceasar(offset)
+elif startmsg == 2:
+	ceasar(- offset)
+else:
+	raise SystemExit
